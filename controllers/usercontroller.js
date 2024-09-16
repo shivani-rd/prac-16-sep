d78d5afaec73542cb1e6ee88bae58d6bc498a244
shivani-rd <shivanichauhan992775@gmail.com>
2024-09-16 15:56:45 +0530

    updation....

---
+++ controllers/usercontroller.js
@@ -0,0 +1,12 @@
+const users = require('../users.json');
+function getUser(req,res){
+    try{
+        res.json(users);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+module.exports = {
+    getUser
+}
